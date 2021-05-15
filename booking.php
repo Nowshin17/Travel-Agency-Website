1c23f0a5b47b492c2eae8266db1e243d709874d6
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-15 19:57:36 +0600

    Update booking.php

--- booking.php
+++ booking.php
@@ -22,7 +22,7 @@ $customer_id = $_SESSION['id'];
   
 
  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <link rel="stylesheet" href="css/new.css">
+        
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
