66286e4fedc5af0045ba260a1fcf1efca6f62d3b
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-15 18:14:06 +0600

    Update delete_customer.php

--- delete_customer.php
+++ delete_customer.php
@@ -4,8 +4,25 @@
     include('connection.php');
      ?>
 
+<head>
+    <meta charset="UTF-8">
+    <title>Add hotel</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  
+    <style> 
+            body {
+            background: #eee;
+            font-family: 'Lato', sans-serif;
+            font-weight: 400;
+            color: #222;
+            font-size: 14px;
+            line-height: 26px;
+            padding: 30px;
+            }
+    </style>
+</head>
 <body>
-    <h1>Delete Customer</h1>
+    <h2>Delete Customer</h2>
     <form action="delete_customer.php" method="post">
         <input type="text" placeholder="cust_name" name="cust_name">
         <button type="submit" name="delete">delete</button>
