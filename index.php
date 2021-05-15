549d45698a0ec67232c638679d5c831189e00c69
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-15 14:09:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -29,9 +29,7 @@
                     <img src="assets/images/phone.png" alt="">
                     <a href="">+88017000000</a>
                 </div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item active">
