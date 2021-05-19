13b4b4337e87a8318c97069cdbe60e3e805b34c9
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-20 04:59:10 +0600

    Update visit_place.php

--- visit_place.php
+++ visit_place.php
@@ -7,7 +7,7 @@
 
 
 <head>
-    <title>Hello, world!</title>
+    <title></title>
 
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -39,38 +39,7 @@
             text-align: center;
         }
 
-    </style>
 
-
-
-
-
-    <div class="button_div"><a href="log_out.php" class="button">Log Out</a></div>
-
-    <style>
-        .button {
-            background-color: #555555;
-            border: black;
-            color: white;
-            padding: 15px 32px;
-            /*text-align: right;*/
-            text-decoration: underline;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
-        }
-
-        .button_div {
-            text-align: center;
-        }
-
-    </style>
-
-    <div class="button_div"><a href="check_book_bill.php" class="button">Check Book and Bill</a></div>
-
-
-    <style>
         table {
             border-collapse: collapse;
         }
@@ -87,18 +56,77 @@
 <body>
 
 
+<!--Header Area Start-->
+<header>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <a class="navbar-brand" href="#">
+                    <h3>TravelBD</h3>
+                </a>
+               
+                
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">User Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="check_book_bill.php">Check bookings and Bills</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="hotel.php">Rate the place</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="hotel.php">Your Recommended places</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link get_started" href="log_out.php">Logout</a>
+                        </li>
+                        
+
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <!--Header Area End-->
+
+
+
+   <!--Banner Area Start-->
+   <section class="banner d-flex align-items-center" style="background-image: url('Images/img_bg_3.jpg');  background-position-y: -100px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="banner_content">
+                        <h1>User profile<br</h1> <h2><b>You are welcome to out Bangladesh</B></h2>
+                        
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Banner Area End-->
+
+
 <div class="container">
        
    
     <div class="col-sm-12">
         <div class="title">
+
+        <!-- <div class="button_div"><a href="log_out.php" class="button">Log Out</a></div>
+        <div class="button_div"><a href="check_book_bill.php" class="button">Check Book and Bill</a></div> -->
         
-            <h1>User profile<br>Place Information</h1>
+    
             
         </div>
     </div>
+    <br>
      <center>
-     <h1>Welcome </h1> 
+     <h2>Choose Your Favorite place and Book now! </h2> 
      </center>
 
     <table class="table table-border">
