9ebd1d98d36f8014be49147f5b21c3388ece1059
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-20 06:19:26 +0600

    Update recommend.php

--- recommend.php
+++ recommend.php
@@ -1,19 +1,6 @@
 <?php
 
-  $dbhost = "localhost";
-  $dbuser = "root";
-  $dbpass = "";
-  $dbname = "recommendation1";
-  
-  
-
-  
-        $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-            }
-
-        echo "Connected successfully";
+include('connection.php');
 
 
 
