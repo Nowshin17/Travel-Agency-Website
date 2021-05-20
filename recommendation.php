cca4be750f55c837174bc6f864b9d933273e9b77
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-20 21:24:09 +0600

    Update recommendation.php

--- recommendation.php
+++ recommendation.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Recommendatiin</title>
+  <title>Recommendation</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
 .error {
