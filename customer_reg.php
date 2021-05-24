1952d57bb74fd536fc79b50fd5cdad695812e751
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-25 03:34:36 +0600

    Update customer_reg.php

--- customer_reg.php
+++ customer_reg.php
@@ -7,76 +7,161 @@ include('connection.php');
 <head>
 
     <meta charset="UTF-8">
-    <title>login</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link rel="stylesheet" href="css/new.css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <title>Registration</title>
+    <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
+    <!-- <link rel="stylesheet" href="css/new.css"> -->
+    <!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+    <link rel="stylesheet" href="css/loginstyle.css"> 
 
-
+<style>
+    .error {
+	color: red;
+    
+}
+</style>
 
 </head>
 
-<body class="user">
+<body>
+    <?php
+    $cust_name=$cust_email=$cust_password=$cust_address=$cust_phonel="";
+    $bad=$clear="";
+    $namerr=$emailerr=$passworderr=$adderr=$noerr="";
+   
+
+    if(isset($_POST['submit']))
+    {
+
+
+
+        if (empty($_POST["cust_name"])){
+            $namerr = "Name is required";
+        }
+        else{
+          $customer_phone = $_POST['cust_phone'];
+        }
+        
+        
+    
+        if (empty($_POST["cust_password"])){
+            $passworderr = "password is required";
+        }
+        else{
+            $customer_password = $_POST['cust_password'];
+        }
+
+
+
+        if (empty($_POST["cust_address"])){
+            $adderr = "address is required";
+        }
+        else{
+            $customer_address = $_POST['cust_address'];
+        }
+        
+
+
+        if (empty($_POST["cust_phone"])){
+
+            $noerr = "Phone no is required";
+        }
+        else{
+    
+            $customer_phone = $_POST['cust_phone'];
+            $regex = "/^\+?(88)?0?1[56789][0-9]{8}/";
+    
+            if (!preg_match($regex,  $customer_phone)) {
+                
+               $noerr = "Valid Bangladeshi mobile no. is required!"; 
+            }
+    
+    
+       
+        }
+        
+
+
+        if (empty($_POST["customer_mail"])){
+            $emailerr = "address is required";
+        }
+        else{
+          
+
+        $customer_mail = $_POST['cust_mail'];
+        
+        $sql_1 = "SELECT * FROM customer WHERE cust_mail = '$customer_mail'";
+        $result = $conn->query($sql_1);
+        
+        if($result->num_rows==0)
+        {
+         
+             $sql = "INSERT INTO customer(cust_name,cust_password,cust_address,cust_mail,cust_phone) VALUES ('$customer_name','$customer_password','$customer_address','$customer_mail','$customer_phone')";
+        
+            if ($conn->query($sql) == TRUE) {
+                echo "New record created successfully<br>";       
+               }
+            
+                header("Location: customer_log.php") ;
+
+           }
+
+
+         else
+         {
+            echo "This mail is already taken<br>";
+         }
+     }
+
+
+
+    }
+
+
+
+    ?>
+
+    
+
+
+<dir class= "form-container">
+	<div class = "user-img"> <img src= ""/> <div>
+	    <ul class= "list" id = h>
     <form action="customer_reg.php" method=post>
 
 
-        <div class="wrapper fadeInDown pad">
-            <div id="formContent">
+    
+
+    <li><input type="text" id="login" class="fadeIn second" name="cust_name" placeholder="Name"></BR>
+    <span class = "error"><?php echo "$namerr"; ?></span></li>
+    <li><input type="text" id="login" class="fadeIn second" name="cust_address" placeholder="Address"></BR>
+    <span class = "error"><?php echo "$adderr"; ?></span></li>
+    <li><input type="text" id="login" class="fadeIn second" name="cust_phone" placeholder="Phone"></BR>
+    <span class = "error"><?php echo "$noerr"; ?></span></li>
+    <li><input type="email" id="login" class="fadeIn second" name="cust_mail" placeholder="Email"></BR>
+    <span class = "error"><?php echo "$emailerr"; ?></span></li>
+
+    <li><input type="password" id="password" class="fadeIn third" name="cust_password" placeholder="Password">
+    </BR>
+    <span class = "error"><?php echo "$passworderr"; ?></span></li>
 
 
-                <div class="fadeIn first">
-                    <h5>User Sign Up</h5>
-                </div>
+    <li><input type="submit" class="fadeIn fourth" name="submit" value="Sign In">
 
-                <input type="text" id="login" class="fadeIn second" name="cust_name" placeholder="Name">
-                <input type="text" id="login" class="fadeIn second" name="cust_address" placeholder="Address">
-                <input type="text" id="login" class="fadeIn second" name="cust_phone" placeholder="Phone">
-                <input type="email" id="login" class="fadeIn second" name="cust_mail" placeholder="Email">
-                <input type="password" id="password" class="fadeIn third" name="cust_password" placeholder="Password">
-                <input type="submit" class="fadeIn fourth" name="submit" value="Sign In">
 
-                <p>Already have an Account?<a href="customer_log.php"> Log In</a></p>
-            </div>
-        </div>
-
+    <li><p>Already have an Account?<a href="customer_log.php"> Log In</a></p></li>
+        
 
 
 
 
     </form>
-    <?php    
-        if(isset($_POST['submit']))
-        {
-            $customer_name = $_POST['cust_name'];
-            $customer_password = $_POST['cust_password'];
-            $customer_address = $_POST['cust_address'];
-            $customer_phone = $_POST['cust_phone'];
-            $customer_mail = $_POST['cust_mail'];
-            
-            $sql_1 = "SELECT * FROM customer WHERE cust_mail = '$customer_mail'";
-            $result = $conn->query($sql_1);
-            
-            if($result->num_rows==0)
-            {
-             
-                 $sql = "INSERT INTO customer(cust_name,cust_password,cust_address,cust_mail,cust_phone) VALUES ('$customer_name','$customer_password','$customer_address','$customer_mail','$customer_phone')";
-            
-                if ($conn->query($sql) == TRUE) {
-                  echo "New record created successfully<br>";       
-                   }
-                
-               header("Location: customer_log.php") ;
+
+    </ul>
+ </dir>
+
+
     
-               }
-             else
-             {
-                echo "This mail is already taken<br>";
-             }
-         }
-    
-    
-        ?>
 </body>
 
 </html>
