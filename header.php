253278d72dcbab412e1926e6d9423cc5f2f0d417
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-25 01:58:24 +0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<?php
+    include('connection.php');
+     ?>
+
+<head>
+    <title>footer</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!--Media CSS-->
+    <link rel="stylesheet" href="assets/css/media.css">
+
+
+    <style>
+      
+.footer {
+  
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 14%;
+   background-color: #5b5b5b;
+   color: white;
+   text-align: center;
+}
+    </style>
+</head>
+
+<body>
+
+<div class="footer">
+ 
+     <header>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <a class="navbar-brand" href="#">
+                    <h3>TravelBD</h3>
+                </a>
+                <div class="top_phn">
+                    <img src="assets/images/phone.png" alt="">
+                    <a href="">+88017000000</a>
+                </div>
+               
+
+                
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="visit_place1.php">Place</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="hotel.php">Hotel</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link get_started" href="customer_log.php">Log In</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link get_started" href="customer_reg.php">Sign Up</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link get_started" href="admin.php">Admin</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <!--Header Area End-->
+    
+</div>
+<br><br>
+
+
+</body>
