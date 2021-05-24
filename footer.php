d10b67186fd3925ea2d538b81a0a39697f79cbeb
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-25 01:58:21 +0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<?php
+    include('connection.php');
+     ?>
+
+<head>
+    <title>footer</title>
+
+
+    <style>
+      
+        .footer {
+  
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: #5b5b5b;
+   color: white;
+   text-align: center;
+}
+    </style>
+</head>
+
+<body>
+
+<div class="footer">
+  <p>Footer</p>
+  <p>Footer</p>
+   <p>Footer</p>
+    
+
+</div>
+
+</body>
