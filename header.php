591093c940c39fa2b0e6f12ac4117aeb59b3aec3
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-26 05:12:51 +0600

    Update header.php

--- header.php
+++ header.php
@@ -5,78 +5,63 @@
      ?>
 
 <head>
-    <title>footer</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!--Media CSS-->
-    <link rel="stylesheet" href="assets/css/media.css">
+    <title>Header</title>
+    <link rel="stylesheet" href="nav_css.css">
 
 
-    <style>
+<style>
+
+        *{
+        margin: 0%;
+        }
+        .topnav {
+            background-color:#333;
+            overflow: hidden;
+            }
+
+        .topnav a {
+            float: left;
+            color:  #f2f2f2 ;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+        
+       
+
+        body {
+            
+            font-family: 'Lato', sans-serif;
+   }
+        .topnav a:hover {
+            background-color: gray;
+            color: black;
+        }
+        
+        .topnav a.active {
+            background-color: #04AA6D;
+            color: white;
+        }
+
       
-.footer {
-  
-   left: 0;
-   top: 0;
-   width: 100%;
-   height: 14%;
-   background-color: #5b5b5b;
-   color: white;
-   text-align: center;
-}
+
     </style>
 </head>
 
 <body>
 
-<div class="footer">
- 
-     <header>
-        <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <a class="navbar-brand" href="#">
-                    <h3>TravelBD</h3>
-                </a>
-                <div class="top_phn">
-                    <img src="assets/images/phone.png" alt="">
-                    <a href="">+88017000000</a>
-                </div>
-               
-
-                
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="visit_place1.php">Place</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="hotel.php">Hotel</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="customer_log.php">Book Now</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link get_started" href="customer_log.php">Log In</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link get_started" href="customer_reg.php">Sign Up</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link get_started" href="admin.php">Admin</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </header>
-    <!--Header Area End-->
-    
+<!-- header -->
+<div class="topnav">
+    <a class="logo" href="#"><div style="color:#04AA6D;">TravelBD</div></a>
+    <a class="active" href="#">Home</a>
+    <a href="visit_place1.php">Place</a>
+    <a href="hotel.php">Hotel</a>
+    <a href="customer_log.php">Booking</a>
+    <a href="customer_log.php">Log In</a>
+    <a href="customer_reg.php">Sign Up</a>
+    <a href="admin.php">Admin</a>
 </div>
-<br><br>
-
+<!-- header -->
 
 </body>
