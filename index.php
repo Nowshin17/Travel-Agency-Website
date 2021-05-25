553b78c48586c984a3d55a89b76f33c3a59cd7b6
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-26 03:32:37 +0600

    Update index.php

--- index.php
+++ index.php
@@ -21,8 +21,9 @@
 session_reset(); 
 ?>
 
-
+<!-- header -->
 <div class="topnav">
+    <a class="logo" href="#"><div style="color:#274A5E;">TravelBD</div></a>
     <a class="active" href="#">Home</a>
     <a href="visit_place1.php">Place</a>
     <a href="hotel.php">Hotel</a>
@@ -31,23 +32,28 @@ session_reset();
     <a href="customer_reg.php">Sign Up</a>
     <a href="admin.php">Admin</a>
 </div>
+<!-- header -->
 
 
-
+<!-- background img -->
 <div class="container">
-    <img src="Images/slide_2.jpg" height="500" alt="Snow" style="width:100%;">
+    <img src="Images/slide_2.jpg" height="630" alt="Snow" style="width:100%;">
     <div class="centered" style="color: white;">
         <h1>Welcome to <br><b><div style="color:#6eef83;"><div  style="font-size:140%;">Bangladesh </div></div></b></h1>
         <p>TravelBD is multipurpose place, with cheap and great Travel packages.<br>TravelBD is perfect place for you to tavel!</p>
     </div>
 </div>
 
+<!-- background img -->
+
+<marquee><b><div style="color:#6eef83;"><div  style="font-size:140%;">TravelBD You can search places. Enjoy Your Holiday!</div></div> 
+</b></marquee>
 
 
-<marquee><b><div style="color:#6eef83;"><div  style="font-size:140%;">TravelBD You can search places. Enjoy Your Holiday!</div></div> </b></marquee>
 
 <div style="text-align: center;">
     <h1>Our features</h1>
+    
     <p>We are here for you to discover the beautiful places of <b>Bangladesh</b></p>
 </div>
 
@@ -74,6 +80,8 @@ session_reset();
         </div>
     </div>
 </div>
+<br><br>
+
 
 <div class="title" style="text-align: center;">
     <h1>Do You Want to know about Bangladesh?</h1>
@@ -88,7 +96,7 @@ session_reset();
     </div>
     <div class="column_2">
         <div style="text-align: center;">
-            <a class="navbar-brand" href="www.bangladesh.com"> <h1>Bangladesh</h1> </a>
+            <a href="www.bangladesh.com"> <h1>Bangladesh</h1> </a><br>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
             <div class="check_text">
             
