1027a44675c3aedbfb6b8dfdc1edf20611e20761
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-26 02:51:39 +0600

    Update index.php

--- index.php
+++ index.php
@@ -18,280 +18,107 @@
 session_reset(); 
 ?>
 
-    <!--Header Area Start-->
-    <header>
-        <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <a class="navbar-brand" href="#">
-                    <h3>TravelBD</h3>
-                </a>
-                <div class="top_phn">
-                    <img src="assets/images/phone.png" alt="">
-                    <a href="">+88017000000</a>
-                </div>
-               
 
-                
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                    
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="visit_place1.php">Place</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="hotel.php">Hotel</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="customer_log.php">Booking</a>
-                            </li>
+<div class="topnav">
+    <a class="active" href="#">Home</a>
+    <a href="visit_place1.php">Place</a>
+    <a href="hotel.php">Hotel</a>
+    <a href="customer_log.php">Booking</a>
+    <a href="customer_log.php">Log In</a>
+    <a href="customer_reg.php">Sign Up</a>
+    <a href="admin.php">Admin</a>
+</div>
+
+<div class="container">
+    <img src="Images/slide_2.jpg" height="500" alt="Snow" style="width:100%;">
+    <div class="centered" style="color: white;">
+        <h1>Welcome to <br><b><div><div  style="font-size:140%;">Bangladesh </div></div></b></h1>
+        <p>TravelBD is multipurpose place, with cheap and great Travel packages.<br>TravelBD is perfect place for you to tavel!</p>
+    </div>
+</div>
 
 
-                            <li class="nav-item">
-                                <a class="nav-link get_started" href="customer_log.php">Log In</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link get_started" href="customer_reg.php">Sign Up</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link get_started" href="admin.php">Admin</a>
-                            </li>
 
-                    </ul>
-                </div>
-            </nav>
+<marquee><b><div style="color:#6eef83;"><div  style="font-size:140%;">TravelBD You can search places. Enjoy Your Holiday!</div></div> </b></marquee>
+
+<div style="text-align: center;">
+    <h1>Our features</h1>
+    <p>We are here for you to discover the beautiful places of <b>Bangladesh</b></p>
+</div>
+
+<div class="row">
+    <div class="column_3">
+        <div style="text-align: center;">
+            <img src="Image/icons8-place-marker-80.png" alt="">
+            <h2>Place</h2>
+            <p>You can search places. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.</p>
         </div>
-    </header>
-    <!--Header Area End-->
+    </div>
+    <div class="column_3">
+        <div style="text-align: center;">
+            <img src="Image/icons8-hotel-80.png" alt="">
+            <h2>Hotel</h2>
+            <p>You can book hotels. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.</p>
+        </div>
+    </div>
+    <div class="column_3">
+        <div style="text-align: center;">
+            <img src="Image/icons8-car-64.png" alt="">
+            <h2>Transportation</h2>
+            <p>You can book seats. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.a.</p>
+        </div>
+    </div>
+</div>
 
+<div class="title" style="text-align: center;">
+    <h1>Do You Want to know about Bangladesh?</h1>
+    <p>Please visit here</p>
+</div>
 
-
-
-    <!--Banner Area Start-->
-    <section class="banner d-flex align-items-center" style="background-image: url('Images/slide_2.jpg');  background-position-y: -100px;">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="banner_content">
-                        <h1>Welcome to <br><b><div style="color:#6eef83;"><div  style="font-size:140%;">Bangladesh </div></div></b></h1>
-                        <p>TravelBD is multipurpose place, with cheap and great Travel packages.<br>TravelBD is perfect place for you to tavel!</p>
-
-                    </div>
-                </div>
+<div class="row">
+    <div class="column_2">
+        <div style="text-align: center;">
+            <img style="height: 100%; width: 100%;" src="Image/man-standing-on-a-rock-1271619.jpg" alt="image error">
+        </div>
+    </div>
+    <div class="column_2">
+        <div style="text-align: center;">
+            <a class="navbar-brand" href="www.bangladesh.com"> <h1>Bangladesh</h1> </a>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+            <div class="check_text">
+            
+                <p><p>Lorem Ipsum is simply dummy text of the printinghe 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p></p>
+                <img src="assets/images/check.png" alt="">
+                <p>Donec id elit non mi porta gravida at eget metus.</p>
             </div>
         </div>
-    </section>
-    <!--Banner Area End-->
+    </div>
+</div>
 
 
-
-    <!-- <marquee><b><div style="color:#6eef83;"><div  style="font-size:140%;">TravelBD You can search places. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</div></div> </b></marquee> -->
-   
-
-    <!--Features Area Start-->
-    <section class="features">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="title">
-                        <h1>Our features</h1>
-                        <p>We are here for you to discover the beautiful places of <b>Bangladesh</b></p>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="features_item text-center">
-                        <img src="Image/icons8-place-marker-80.png" alt="">
-                        <h2>Place</h2>
-                        <p>You can search places. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.</p>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="features_item text-center">
-                        <img src="Image/icons8-hotel-80.png" alt="">
-                        <h2>Hotel</h2>
-                        <p>You can book hotels. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.</p>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="features_item text-center">
-                        <img src="Image/icons8-car-64.png" alt="">
-                        <h2>Transportation</h2>
-                        <p>You can book seats. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.a.</p>
-                    </div>
-                </div>
-                <div class="w-100"></div>
-                <div class="col-sm">
-                    <div class="divider"></div>
-                </div>
-                <div class="w-100"></div>
-
-
-            </div>
-        </div>
-    </section>
-    <!--Features Area End-->
-
-
-
-
-
-
-    <!--Perfect Template Area Start-->
-    <section class="perfect">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="title">
-                        <h1>Do You Want to know about Bangladesh?</h1>
-                        <p>Please visit here</p>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <a herf="www.bangladesh.com" > <img class="img-fluid" src="Image/man-standing-on-a-rock-1271619.jpg" alt="image error">
-                </div></a>
-                <div class="col-sm-6">
-                <a class="navbar-brand" href="www.bangladesh.com"> <h1>Bangladesh</h1> </a>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
-                    <div class="check_text">
-                    
-                        <p><p>Lorem Ipsum is simply dummy text of the printinghe 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p></p>
-                        <img src="assets/images/check.png" alt="">
-                        <p>Donec id elit non mi porta gravida at eget metus.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-<br><br>
-        
-    </section>
-    <!--Perfect Template Area End-->
-
-
-
-
-
-
-    <!--Banner Area Start-->
-    <section class="banner d-flex align-items-center" style="background-image: url('Images/slide_2.jpg');  background-position-y: -100px;">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="banner_content">
-                        <center>
-                        <h1>Welcome to <br><b><div style="color:#6eef83;"><div  style="font-size:140%;">Bangladesh </div></div></b></h1>
-                        <p>TravelBD is multipurpose place, with cheap and great Travel packages.<br>TravelBD is perfect place for you to tavel!</p>
-</center>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--Banner Area End-->
-
-
-
-      <!--Perfect Template Area Start-->
-      <section class="perfect">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="title">
-                        <h1>Do You Want to know about Bangladesh?</h1>
-                        <p>Please visit here</p>
-                    </div>
-                </div>
-
-
-                        <div class="gallery">
-                            
-                            <a target="_blank" href="img_5terre.jpg">
-                                <img src="images/hotel-1.jpg" alt="Cinque Terre" width="600" height="400">
-                            </a>
-                            <div class="desc">Add a description of the image here</div>
-                            </div>
-
-                            
-
-                            <div class="gallery">
-                            <a target="_blank" href="img_lights.jpg">
-                                <img src="img_lights.jpg" alt="Northern Lights" width="600" height="400">
-                            </a>
-                            <div class="desc">Add a description of the image here</div>
-                            </div>
-
-                            <div class="gallery">
-                            <a target="_blank" href="img_mountains.jpg">
-                                <img src="img_mountains.jpg" alt="Mountains" width="600" height="400">
-                            </a>
-                            <div class="desc">Add a description of the image here</div>
-                            </div>
-
-
-
-
-
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-<br><br>
-        
-    </section>
-    <!--Perfect Template Area End-->
-
-
-                    
-
-<br><br>
-
-
-
-   
-
-
-    <!--Call to Action Area Start -->
-    <section class="call_to_action">
-        
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-sm-8">
-                    <div class="title">
-                        <h1>Need Tour Booking?</h1><br>
-                    </div>
-
-                    <!-- <div class="col-sm-4 align-self-center">
-                    <a href="customer_reg.php">
-                        <button class="btn custom_btn ">Book Now
-                        </button> -->
-                </div>
-                
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--Call to Action Area End-->
-
-
+<div class="row">
+    <div class="column_3">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/hotel-1.jpg" alt="Cinque Terre" width="100%" height="100%">
+            </a>
+            <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="column_3">
+            <a target="_blank" href="img_lights.jpg">
+                <img src="img_lights.jpg" alt="Northern Lights" width="600" height="400">
+            </a>
+            <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="column_3">
+            <a target="_blank" href="img_mountains.jpg">
+                <img src="img_mountains.jpg" alt="Mountains" width="600" height="400">
+            </a>
+            <div class="desc">Add a description of the image here</div>
+    </div>
+</div>
    
 
 <br><br>
-<!-- footet -->
-
-<?php
-    include('footer.php');
-     ?>
-
-<!-- footer section -->
-
 
 </body>
 
