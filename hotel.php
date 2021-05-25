bddf05643517bb907133f6e147ccbcdbd6c4ca6b
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-26 05:12:54 +0600

    Update hotel.php

--- hotel.php
+++ hotel.php
@@ -1,54 +1,91 @@
-
-
-
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <?php
     include('connection.php');
      ?>
 
 <head>
-
-    <meta charset="UTF-8">
-    <title>Hotel</title>
-
+    <title>hotel</title>
     <meta charset="utf-8">
-    <title>See hotels!</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    
-    <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto:300,400" rel="stylesheet">
+    <title>Find you trip!</title>
+    <link rel="icon" href="Image/logo.png">
+
 
     <style>
+            *{
+             margin: 0%;
+             }
 
-        body {
-        background: #eee;
-        font-family: 'Lato', sans-serif;
-        font-weight: 400;
-        color: #222;
-        font-size: 14px;
-        line-height: 26px;
-        
-        }
-    </style>
-   
+            body{
+            background: #eee;
+            font-family: 'Lato', sans-serif;
+            font-weight: 400;
+            color: #222;
+            font-size: 14px;
 
+            }
+            .a {
+            line-height: 26px;
+            padding: 10px;
+            margin: 120px;
+            } 
+
+            img:hover {
+            opacity: 0.5;
+            }
+            div.card {
+               
+                box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+                transition: 0.3s;
+                width: 230px;
+                height: auto;
+            }
+            div.card:hover {
+                box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+            }
+
+            div.container {
+                padding: 2px 10px;
+                text-align: center;
+            }
+            .column {
+                float: left;
+                width: 30%;
+                padding: 20px;
+            }
+            .row:after {
+                content: "";
+                display: table;
+                clear: both;
+            }
+            button{
+                padding: 10px;
+                background-color: #04AA6D;
+                
+            }
+            
+}
+</style>
 </head>
+
+<?php
+    include('header.php');
+     ?>
 
 <body>
     
-<?php
     
-     ?>
+<div class="a"> 
+<center>
+    <div style="margin-right:10%;"><h1 style="color: #274A5E;"><b>--- Our Available Hotels ---<b></h1></div>
+    </center>
+      
+      
+        <div class="row">
+        <br><br>
+  
+  <?php
 
-    <h1 style="text-align:center;">Our Available Hotels</h1>
-    <hr>
-
-    <div class="container">
-
-        <div class="row mb-2">
-            
-            <?php
             $sql = "SELECT * FROM hotel";
             $result = $conn->query($sql);
             
@@ -56,46 +93,40 @@
             while($row = $result->fetch_assoc()) {
                 
             ?>
-
-
-            <div class="col-md-6">
-                <div class="card flex-md-row mb-4 box-shadow h-md-250">
-                    <div class="card-body ">
-                        <h3 class="mb-0">
-                        <?php echo $row['hotel_name']; ?>
-                        </h3>
-
-                         Hotel cost : <?php echo $row['room_cost']; ?> <br>
-                        Available seats : <?php echo $row['avl_room']; ?>
-                        
-
-                        <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-
-                    </div>
-                    <img class="card-img-right flex-auto d-none d-md-block" alt="Thumbnail [200x250]" style="width: 200px;" src="Image/view-of-tourist-resort-338504.jpg" data-holder-rendered="true">
-                </div>
-            </div>
-
-            <?php
             
-        }
-        }
-        ?>
+
+            <div class="column">
+                    <div class="card">
+                        <img src="Image/view-of-tourist-resort-338504.jpg" height="200" width="230">
+        
+                            <div class="container">
+                                <h2><b><?php echo $row['hotel_name']; ?></b></h2><br>
+                                <b>Hotel cost :</b> <?php echo $row['room_cost']; ?> <br>
+                                <b>Available seats :</b> <?php echo $row['avl_room']; ?>
+                                <br><br>
+                            
+                            </div>
+                         </div>
+                     </div>
+                 <?php
             
-           
+                     }
+                  }
+                ?>
 
-
-
-
+                 </div>
+             </div>
 
         </div>
 
+        
         <a href="customer_log.php">
-            <button class="btn custom_btn" style="margin-left:500px;">Book Now</button>
-
+            <button style="margin-left:700px;">Book Now</button>
         </a>
+        <br><br>
+         
 
-    </div>
+   
 
     
 
@@ -107,4 +138,3 @@
 
 </body>
 
-</html>
