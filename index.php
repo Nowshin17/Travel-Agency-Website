73328c72507ebdb7c6356ab445d001c215e5d77f
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-26 12:24:25 +0600

    Update index.php

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ session_reset();
     </div>
     <div class="column_2">
         <div style="text-align: center;">
-            <a href="www.bangladesh.com"> <h1>Bangladesh</h1> </a><br>
+            <a href="https://en.wikipedia.org/wiki/Bangladesh"> <h1>Bangladesh</h1> </a><br>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
             <div class="check_text">
             
