f58728700b253c73271957e703361ee96551bd6c
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-26 15:45:45 +0600

    Update show_customer.php

--- show_customer.php
+++ show_customer.php
@@ -3,7 +3,6 @@
     include('connection.php');
 ?>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <title>Add hotel</title>
@@ -19,25 +18,22 @@
             line-height: 26px;
             padding: 30px;
             }
+            table {
+            border-collapse: collapse;
+            }
+            table, th, td {
+            border: 1px solid black;
+            }
     </style>
 </head>
-    <body>
-    <center>
-        <h1> Resgistered Users</h1> </center>
-         <style>
-        table {
-         border-collapse: collapse;
-          }
 
-         table, th, td {
-         border: 1px solid black;
-          }
-        </style>
+    <body>
+    <center> <h1> Resgistered Users</h1> </center>
+       
         <table class = "table table-border">
             <tr>
                 <th>customer_name</th>
-                <th>customer_id</th>
-               
+                <th>customer_id</th> 
                 <th>customer_address</th>
                 <th>customer_mail</th>
                 <th>customer_phone</th>
@@ -53,7 +49,6 @@
             <tr>
                 <td><?php echo $row['cust_name']; ?></td>
                 <td><?php echo $row['cust_id']; ?></td>
-               
                 <td><?php echo $row['cust_address']; ?></td>
                 <td><?php echo $row['cust_mail']; ?></td>
                 <td><?php echo $row['cust_phone']; ?></td>
