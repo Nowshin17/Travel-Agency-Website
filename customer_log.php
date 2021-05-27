1b28059fa8a4f1c20da37aa3c2b189061b837232
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-28 04:14:56 +0600

    Update customer_log.php

--- customer_log.php
+++ customer_log.php
@@ -6,15 +6,27 @@ session_start();
 <html>
 
 <head>
+
     <meta charset="UTF-8">
     <title>login</title>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="css/new.css">
-   
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<style>
+
+body {
+  background-image: url('Image/unnamed.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
 
 </head>
 
-<body class="user">
+<body>
 <?php
 $err="";
 
