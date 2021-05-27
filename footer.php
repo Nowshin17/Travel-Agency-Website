9e6a490cc1b687d4df9323a0d0fd6702df50c10f
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-28 01:04:12 +0600

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.footer {
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: #008060;
+   color: white;
+   text-align: center;
+   display: flex;
+   padding-left: 10%;
+  /* height: 100px;*/
+   padding: 4%;
+
+}
+footer.p{
+padding: 10px;
+margin: 10px;
+}
+</style>
+</head>
+<body>
+
+<div class="footer">
+  <p><b>TravelBD <b> </p> <br>
+  <p>Contact us: Travel.BD</p>
+  <p>Thank You fot visiting us</p>
+  
+  
+</div>
+
+</body>
+</html> 
