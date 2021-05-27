ba8a4bb74b51c06c9c0f601a2eb71c0036d5875d
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2021-05-28 01:04:15 +0600

    Update customer_reg.php

--- customer_reg.php
+++ customer_reg.php
@@ -8,11 +8,11 @@ include('connection.php');
 
     <meta charset="UTF-8">
     <title>Registration</title>
-    <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
-    <!-- <link rel="stylesheet" href="css/new.css"> -->
-    <!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
-    <link rel="stylesheet" href="css/loginstyle.css"> 
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
+    <link rel="stylesheet" href="css/new.css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
 
 <style>
     .error {
